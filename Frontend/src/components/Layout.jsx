3be15bf1fdf366bf522bb7a7ec0bc26725c9a6ac
challatoor murali <challatoor murali.gmail.com>
2026-07-30 15:01:49 +0530

    medical

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,24 +1,22 @@
+import React from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 function Layout({ children }) {
-  return (
-    <div className="app-layout">
+return ( <div className="app-layout"> <Sidebar />
 
-      <Sidebar />
+```
+  <div className="main-section">
+    <Header />
 
-      <div className="main-section">
+    <main className="page-content">
+      {children}
+    </main>
+  </div>
+</div>
+```
 
-        <Header />
-
-        <main className="page-content">
-          {children}
-        </main>
-
-      </div>
-
-    </div>
-  );
+);
 }
 
 export default Layout;
