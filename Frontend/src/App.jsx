fcd9ade609b3d271811f127a1ea414a83dd489b8
challatoor murali <challatoor murali.gmail.com>
2026-07-30 21:03:20 +0530

    medical

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,91 +1,13 @@
-import React, { useState } from "react";
+// 
 
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Users from "./pages/Users";
-import Medicines from "./pages/Medicines";
-import Inventory from "./pages/Inventory";
-import Suppliers from "./pages/Suppliers";
-import Orders from "./pages/Orders";
-import Billing from "./pages/Billing";
-
+import React from "react";
 
 function App() {
-
-  const [currentPage, setCurrentPage] = useState("Dashboard");
-
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    !!localStorage.getItem("token")
-  );
-
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-
-  const renderPage = () => {
-
-    switch (currentPage) {
-
-      case "Users":
-        return <Users />;
-
-      case "Medicines":
-        return <Medicines />;
-
-      case "Inventory":
-        return <Inventory />;
-
-      case "Suppliers":
-        return <Suppliers />;
-
-      case "Orders":
-        return <Orders />;
-
-      case "Billing":
-        return <Billing />;
-
-      default:
-        return <Dashboard />;
-
-    }
-
-  };
-
-
-  if (!isLoggedIn) {
-    return <Login onLogin={handleLogin} />;
-  }
-
-
   return (
-
-    <div className="app-container">
-
-      <Sidebar
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-
-
-      <main className="main-content">
-
-        <Header />
-
-        {renderPage()}
-
-      </main>
-
-
-    </div>
-
+    <h1>
+      React Working
+    </h1>
   );
-
 }
-
 
 export default App;
