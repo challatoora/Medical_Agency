b780ba40c0ee3d4d290acc304d05fe3a2732f740
challatoor murali <challatoor murali.gmail.com>
2026-07-30 12:54:09 +0530

    medical

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,45 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
-import Medicines from "./pages/Medicines";
-import Inventory from "./pages/Inventory";
-import Suppliers from "./pages/Suppliers";
-import Orders from "./pages/Orders";
-import Billing from "./pages/Billing";
-import Users from "./pages/Users";
 
 import "./App.css";
 
 function App() {
-return (
-<BrowserRouter>
-<div className="app-container">
-<Sidebar />
-
-    <div className="main-content">
-      <Navbar />
-
-      <main className="page-content">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/medicines" element={<Medicines />} />
-          <Route path="/inventory" element={<Inventory />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/billing" element={<Billing />} />
-          <Route path="/users" element={<Users />} />
-        </Routes>
-      </main>
-    </div>
-  </div>
-</BrowserRouter>
-
-);
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
 }
 
 export default App;
