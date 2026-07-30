13be04d2343effa8a6f71e4567f1efca0fbc3d6b
challatoor murali <challatoor murali.gmail.com>
2026-07-30 16:20:02 +0530

    medical

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,4 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 import React from "react";
 import {
   LayoutDashboard,
