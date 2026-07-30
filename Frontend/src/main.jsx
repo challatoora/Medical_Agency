dfae2da776ea5951b8f524a7bab93fe0a3a6254c
challatoor murali <challatoor murali.gmail.com>
2026-07-30 21:31:19 +0530

    medical

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+
+console.log("MAIN JSX LOADED");
+
+function Test() {
+  return (
+    <h1 style={{color:"red"}}>
+      REACT IS WORKING
+    </h1>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Test />
 );
