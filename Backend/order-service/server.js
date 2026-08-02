3a686f88f2e6137a0c70a684629af4caae5ef567
challatoor murali <challatoor murali.gmail.com>
2026-08-02 16:02:30 +0530

    git_actions

--- Backend/order-service/server.js
+++ Backend/order-service/server.js
@@ -1,10 +1,25 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+
+const orderRoutes = require("./routes/orderRoutes");
+
+const app = express();
 
 const PORT = 5004;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
+// Order routes
+app.use("/api/orders", orderRoutes);
 
-    console.log(`Order Service Running on Port ${PORT}`);
+// Health check
+app.get("/", (req, res) => {
+res.send("Order Service Running");
+});
 
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
+console.log(`Order Service running on port ${PORT}`);
 });
