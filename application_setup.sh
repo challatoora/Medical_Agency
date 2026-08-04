5c6a6ee3bec50c448ae00f1e72e477b6f0716ced
challatoor murali <challatoor murali.gmail.com>
2026-08-04 14:13:52 +0530

    medical

--- application_setup.sh
+++ application_setup.sh
@@ -16,8 +16,12 @@ npm install --save-dev nodemon
 
 echo "Setup Completed Successfully."
 
+sudo dnf module enable nodejs:20 -y
+sudo dnf install nodejs -y
+npm install
 npm install lucide-react
 npm install lucide-react recharts
+
 
 sudo dnf install -y mongodb-org
 sudo systemctl enable mongod
@@ -27,8 +31,17 @@ sudo systemctl status mongod
 
 sudo dnf module enable redis:7 -y
 sudo dnf install redis -y
-sudo systemctl start redis7
-sudo systemctl status redis7
+sudo systemctl start redis
+sudo systemctl status redis
+
+
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo systemctl status docker
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'Root@123';
 # FLUSH PRIVILEGES;
 # EXIT;
